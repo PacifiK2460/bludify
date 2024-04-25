1335db1c8c32b643ac378607a281d4a58fa1e3a7
Santiago de la cruz Martinez Lara <chago1929@hotmail.com>
2024-04-24 19:41:23 -0600

    Initial Commit

---
+++ app/src/main/java/dev/santiago_lara/bludify/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package dev.santiago_lara.bludify.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/dev/santiago_lara/bludify/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package dev.santiago_lara.bludify.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
